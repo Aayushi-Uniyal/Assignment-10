956321fc6042159093e3a37bbfedce81bb5c3c0a
Aayushi-Uniyal <auniyal_be24@thapar.edu>
2025-11-30 13:50:15 +0530

    Implement function to find first non-repeating element

---
+++ Q4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int firstNonRepeating(int arr[], int n) {
+    int freq[10000] = {0};
+
+    for (int i = 0; i < n; i++)
+        freq[arr[i]]++;
+
+    for (int i = 0; i < n; i++)
+        if (freq[arr[i]] == 1)
+            return arr[i];
+
+    return -1;
+}
+
+int main() {
+    int arr[] = {4, 5, 1, 2, 0, 4};
+    int n = 6;
+    cout << firstNonRepeating(arr, n);
+}
